20511c46239192d254f4386f84f5073f640f8ac0
iamindika <ikolamba2@gmail.com>
2022-10-20 03:26:50 -0400

    add rating and submit state and handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Rating from "./components/Rating"
 
 function App() {
+  const [ rating, setRating ] = useState(null);
+  const [ isSubmit, setIsSubmit ] = useState(false);
+
+  const handleClick = (option) => {
+    if(option !== rating) {
+      setRating(option);
+    }
+  }
+
   return (
     <>
       <Rating 
-        selected={2}
-        onClick={() => {}}
+        selected={rating}
+        onClick={handleClick}
       /> 
     </>
   )
